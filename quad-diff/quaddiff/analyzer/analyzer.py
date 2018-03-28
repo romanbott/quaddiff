b1fee1bc061f7be2211b5fc4d53ace6368296ee9
Santiago Martínez <santiago.mbal@gmail.com>
2018-03-28 13:25:11 -0600

    binary search for critical trajectories

--- quad-diff/quaddiff/analyzer/analyzer.py
+++ quad-diff/quaddiff/analyzer/analyzer.py
@@ -67,14 +67,50 @@ class Analyzer(object):
             for init_p, end_p in zip(init_points, end_points)]
 
         critical = {}
-        solved = []
+        unsolved = [0, 1, 2]
         while True:
-            for i in range(3):
+            # Check if solved
+            for i in unsolved:
                 t1, t2 = trajectories[i]
                 if t1.converges(zero, distance_2limit=self.distance_2limit):
-                    pass
+                    solved.remove(i)
+                    critical[(t1.basepoint, phase)] = t1
+                elif t2.converges(zero, distance_2limit=self.distance_2limit):
+                    solved.remove(i)
+                    critical[(t2.basepoint, phase)] = t2
+                if abs(phase_intervals[i][0] - phase_intervals[i][1]) <= self.stoping_distance:
+                    solved.remove(i)
+                    critical[(t1.basepoint, phase)] = t1
 
-            break
+            if len(solved) == 0:
+                break
+
+            new_args = []
+            for i in unsolved:
+                middle_phase = (phase_intervals[i]/2 +  phase_intervals[i][1])/2
+                new_args.append((zero + cm.rect(self.epsilon, middle_phase), phase))
+
+            new_trajectories = solver.parallel_calculate(new_args, progressbar=False)
+
+            for i, arg in zip(unsolved, new_args):
+                middle_trajectory = new_trajectories[arg]
+                t1, t2 = trajectories[i]
+                if same_zone(t1, middle_trajectory):
+                    trajectories[i][0] = middle_trajectory
+                    phase_intervals[i][0] = (phase_intervals[i]/2 +  phase_intervals[i][1])/2
+                else:
+                    trajectories[i][1] = middle_trajectory
+                    phase_intervals[i][1] = (phase_intervals[i]/2 +  phase_intervals[i][1])/2
+
+        return solved
+
+    def critical_trajectories_zeros_2(self, phase):
+        trayectories = {}
+        for zero in self.qd.zeros:
+            new_trajectories = self.critical_trajectories_zero_2(zero, phase)
+            trayectories.update(new_trajectories)
+
+        return trajectories
 
     def critical_trajectories_zeros(self, phase):
         trayectories = {}
@@ -83,3 +119,24 @@ class Analyzer(object):
             trayectories.update(new_trajectories)
 
         return trayectories
+
+def same_zone(trajectory1, trajectory2, close=0.01):
+    first1 = trajectory1[0]
+    first2 = trajectory2[0]
+    last1 = trajectory1[-1]
+    last2 = trajectory2[-1]
+
+    f1f2 = abs(first1 - first2) <= close
+    f1l2 = abs(first1 - last2) <= close
+    l1f2 = abs(last1 - first2) <= close
+    l1l2 = abs(last1 - last2) <= close
+
+    if f1f2:
+        return l1l2
+    elif f1l2:
+        return l1f2
+    else:
+        return False
+
+
+
