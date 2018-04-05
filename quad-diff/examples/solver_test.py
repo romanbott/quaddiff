6777330c2daaad3d213a526db3e2c0f1fb39f654
Santiago Martínez <santiago.mbal@gmail.com>
2018-04-05 15:08:39 -0500

    solver test clean

--- quad-diff/examples/solver_test.py
+++ quad-diff/examples/solver_test.py
@@ -1,3 +1,10 @@
+"""Testing refactored trayectory solver"""
+# pylint: disable=no-member
+# pylint: disable=wrong-import-position
+# pylint: disable=import-error
+
+from __future__ import print_function
+
 import os
 import sys
 sys.path.insert(
@@ -5,56 +12,74 @@ sys.path.insert(
 
 from time import time
 import numpy as np
+from tqdm import tqdm
 
 from quaddiff.core.trajectory import calculate_ray
 from quaddiff.core.trajectory import calculate_ray_2
 import quaddiff as qd
 
-quad = qd.QuadraticDifferential()
+N_ZEROS = 4
+N_SMPL_POLES = 4
+N_DBL_POLES = 6
+N_LOOPS = 300
 
-# Number of loops
-N_LOOPS = 100
 
-optimized_time = 0
-legacy_time = 0
-for _ in range(N_LOOPS):
-    print("Building new Quadratic Differential")
-    # Build quadratic differential
-    N_ZEROS = 2
-    ZEROS = np.random.uniform(-5, 5, size=[N_ZEROS, 2])
-    
-    N_SMPL_POLES = 2
-    SMPL_POLES  = np.random.uniform(-5, 5, size=[N_SMPL_POLES, 2])
-    
-    N_DBL_POLES = 2
-    DBL_POLES = np.random.uniform(-5, 5, size=[N_DBL_POLES, 2])
-    
-    for zero in ZEROS:
-        quad.add_zero(complex(*zero))
-    
-    for pole in SMPL_POLES:
-        quad.add_smplpole(complex(*pole))
-    
-    for pole in DBL_POLES:
-        quad.add_dblpole(complex(*pole))
-    
-    plotpoint = complex(*np.random.uniform(-5, 5, size=[2]))
+def main():
+    """Calculate mean processing times for solver methods"""
+    quad = qd.QuadraticDifferential()
 
-    print("Calculating with new code")
-    t = time()
-    calculate_ray(plotpoint, quad)
-    optimized_time += time() - t
+    opt_time = 0
+    old_time = 0
+    opt_errs = 0
+    old_errs = 0
+    for _ in tqdm(range(N_LOOPS)):
+        # build quadratic differential
+        zeros = np.random.uniform(-5, 5, size=[N_ZEROS, 2])
+        smpl_poles = np.random.uniform(-5, 5, size=[N_SMPL_POLES, 2])
+        dbl_poles = np.random.uniform(-5, 5, size=[N_DBL_POLES, 2])
 
-    print("Calculating with old code")
-    t = time()
-    plotpoint = complex(*np.random.uniform(-5, 5, size=[2]))
-    calculate_ray_2(plotpoint, quad)
-    legacy_time += time() - t
+        for zero in zeros:
+            quad.add_zero(complex(*zero))
 
-mesage = ''' ======== RESULTS =======
-|{:^10}|{:^10}|{:^10}|{:^10}|
-----------------------------------------------
-|{:^10}|{:^10}|{:^10}|{:^10}|
-----------------------------------------------
-'''.format("new-total", "new-per", "old-total", "old-per", round(optimized_time, 5), round(optimized_time / N_LOOPS, 5), round(legacy_time, 5), round(legacy_time / N_LOOPS, 5))
-print(mesage)
+        for pole in smpl_poles:
+            quad.add_smplpole(complex(*pole))
+
+        for pole in dbl_poles:
+            quad.add_dblpole(complex(*pole))
+
+        plotpoint = complex(*np.random.uniform(-5, 5, size=[2]))
+
+        start = time()
+        try:
+            calculate_ray(plotpoint, quad)
+            opt_time += time() - start
+        except:
+            opt_errs += 1
+
+        start = time()
+        try:
+            calculate_ray_2(plotpoint, quad)
+            old_time += time() - start
+        except:
+            old_errs += 1
+
+    # Message header
+    message = ("=" * 19) + "RESULTS" + ("=" * 19) + "\n"
+    message += "|{:^10}|{:^10}|{:^10}|{:^10}|\n".format("version", "total", "per", "errors")
+    message += "|" + "-" * 43 + "|" + "\n"
+
+    # New version information
+    opt_per = opt_time / (N_LOOPS - opt_errs)
+    message += "|{:^10}|{:^10.4f}|{:^10.4f}|{:^10}|\n".format("new", opt_time, opt_per, opt_errs)
+    message += "|" + "-" * 43 + "|" + "\n"
+
+    # Old version information
+    old_per = old_time / (N_LOOPS - old_errs)
+    message += "|{:^10}|{:^10.4f}|{:^10.4f}|{:^10}|\n".format("old", old_time, old_per, old_errs)
+    message += "-" * 45 + "\n"
+    message += "N_LOOPS = {}".format(N_LOOPS)
+
+    print(message)
+
+if __name__ == "__main__":
+    main()
